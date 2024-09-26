d251a61191d04d8d7a95da891eaa94df6ceca5a6
Bulbo <gabrielshot1@gmail.com>
2024-09-26 20:43:59 -0300

    atualizado projeto

--- js/character.js
+++ js/character.js
@@ -1,1 +1,8 @@
-const fetchData = async () => {};
+const fetchData = async (id) => {
+  const baseUrl = "https://rickandmortyapi.com/api/character/";
+  try {
+    const data = await fetch(baseUrl + id);
+  } catch (err) {
+    console.log(err);
+  }
+};
