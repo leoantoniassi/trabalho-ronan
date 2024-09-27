85da0ad32f324f73c64ad9c242fa14f7401cc6c2
Bulbo <gabrielshot1@gmail.com>
2024-09-26 22:23:46 -0300

    pegado propriedades do character

--- js/character.js
+++ js/character.js
@@ -1,14 +1,40 @@
+const url = window.location.href;
+const params = new URLSearchParams(window.location.search);
+
 const fetchData = async (id) => {
   const baseUrl = "https://rickandmortyapi.com/api/character/";
   try {
-    const data = await fetch(baseUrl + id);
+    const response = await fetch(baseUrl + id);
+    return await response.json();
   } catch (err) {
     console.log(err);
   }
 };
 
-const url = window.location.href;
+const id = params.get("id");
 
-const params = new URLSearchParams(window.location.search);
+const HtmlNome = document.querySelector(".nome");
+const HtmlStatus = document.querySelector(".status");
+const HtmlSpecie = document.querySelector(".specie");
+const HtmlGender = document.querySelector(".gender");
+const HtmlOrigin = document.querySelector(".origem");
 
-const nome = params.get("id");
+console.log("Elemento HTML:", HtmlNome);
+
+const updateHtml = async () => {
+  const data = await fetchData(id);
+  console.log("Dados recebidos:", data);
+
+  const episodes = data.episodes;
+  console.log(episodes);
+
+  if (data) {
+    HtmlNome.textContent = data.name;
+    HtmlStatus.textContent = data.status == "Alive" ? "Vivo" : "Morto";
+    HtmlSpecie.textContent = data.species;
+    HtmlGender.textContent = data.gender == "Male" ? "Homem" : "Mulher";
+    HtmlOrigin.textContent = data.origin.name;
+  }
+};
+
+updateHtml();
