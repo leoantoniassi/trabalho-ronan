7e0adefd4b07dda237bac35b07c2f0bbd54cc6fd
Bulbo <gabrielshot1@gmail.com>
2024-09-30 18:04:49 -0300

    perfil de personagem

--- js/character.js
+++ js/character.js
@@ -19,26 +19,36 @@ const HtmlSpecie = document.querySelector(".specie");
 const HtmlGender = document.querySelector(".gender");
 const HtmlOrigin = document.querySelector(".origem");
 const HtmlEpisodes = document.querySelector(".episodes");
-
-
+const HtmlPick = document.querySelector(".charimg");
 
 const updateHtml = async () => {
   const data = await fetchData(id);
-
 
   const episodes = data.episode;
   console.log(episodes);
 
   if (data) {
+    console.log(data);
+    HtmlPick.src = data.image;
     HtmlNome.textContent = data.name;
     HtmlStatus.textContent = data.status == "Alive" ? "Vivo" : "Morto";
+
+    if (data.status == "Alive") {
+      HtmlStatus.classList.add("alive");
+    } else {
+      HtmlStatus.classList.add("dead");
+    }
+
     HtmlSpecie.textContent = data.species;
     HtmlGender.textContent = data.gender == "Male" ? "Homem" : "Mulher";
-    HtmlOrigin.textContent = data.origin.name;
+    HtmlOrigin.textContent +=
+      data.origin.name == "unknown" ? "disconhecido" : data.origin.name;
     HtmlEpisodes.innerHTML = "";
 
     episodes.forEach((element) => {
-      HtmlEpisodes.innerHTML += `<span> ${element} </span>`;
+      HtmlEpisodes.innerHTML += `<span>Episodeo: ${element
+        .split("/")
+        .pop()} </span>`;
     });
   }
 };
